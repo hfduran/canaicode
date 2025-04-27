f06a6de9823f3e04678c23fcc0202b4da74bedc2
Henrique Fuga Duran <henriqueduran15@gmail.com>
2025-04-27 19:33:06 -0300

    Revert "feat: create entities"

--- src/domain/entities/copilot_chat_metrics.py
+++
@@ -1,8 +0,0 @@
-from src.domain.entities.copilot_metrics import CopilotMetrics
-
-
-class CopilotChatMetrics(CopilotMetrics):
-    total_users: int
-    total_chats: int
-    copy_events: int
-    insertion_events: int
--- src/domain/entities/entity.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class Entity(BaseModel):
-    id: str | None = None
