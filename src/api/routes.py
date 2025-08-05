838d5ef24d223a29b877da6a5d9160a4b586d335
Carlos Caliman <carloshmc25@gmail.com>
2025-08-05 18:56:45 -0300

    feat: read csv metrics

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,11 +1,13 @@
 from datetime import datetime
+import io
 from typing import Any, Dict, List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, File, UploadFile
 from sqlalchemy.orm import Session
 
 from src.config.config import CONFIG
 from src.consumers.gh_copilot.gh_copilot_consumer import GhCopilotConsumer
+from src.consumers.git_metrics_csv.git_metrics_csv_consumer import GitCommitMetricsCsvConsumer
 from src.consumers.git_repo_consumer import GitRepoConsumer
 from src.domain.entities.commit_metrics import CommitMetrics
 from src.domain.use_cases.dtos.calculated_metrics import CalculatedMetrics, CopilotMetricsByLanguage, CopilotMetricsByPeriod, CopilotUsersMetrics
@@ -15,6 +17,7 @@ from src.domain.use_cases.get_copilot_metrics_by_language_use_case import GetCop
 from src.domain.use_cases.get_copilot_metrics_by_period_use_case import GetCopilotMetricsByPeriodUseCase
 from src.domain.use_cases.get_copilot_metrics_use_case import GetCopilotMetricsUseCase
 from src.domain.use_cases.get_copilot_users_metrics_use_case import GetCopilotUsersMetricsUseCase
+from src.domain.use_cases.get_csv_commit_metrics_use_case import GetCsvCommitMetricsUseCase
 from src.infrastructure.database.connection.database_connection import SessionLocal
 from src.infrastructure.database.raw_commit_metrics.postgre.raw_commit_metrics_repository import RawCommitMetricsRepository
 from src.infrastructure.database.raw_copilot_chat_metrics.postgre.raw_copilot_chat_metrics_repository import RawCopilotChatMetricsRepository
@@ -31,7 +34,7 @@ def get_db() -> Any:
         db.close()
 
 
-@router.get("/get_temporary/commit_metrics/{team_name}")
+@router.get("/commit_metrics/upload/{team_name}")
 def get_commit_metrics(
     team_name: str,
     date_string: str = "",
@@ -43,7 +46,7 @@ def get_commit_metrics(
     return response
 
 
-@router.get("/get_temporary/copilot_metrics/{team_name}")
+@router.get("/copilot_metrics/upload/{team_name}")
 def get_copilot_metrics(
     team_name: str,
     date_string: str = "",
@@ -52,6 +55,17 @@ def get_copilot_metrics(
     date = datetime.strptime(date_string, "%Y-%m-%d").date()
     get_copilot_metrics_use_case = set_get_copilot_metrics_dependencies(db)
     response = get_copilot_metrics_use_case.execute(date, team_name)
+    return response
+
+
+@router.post("/commit_metrics/upload_csv")
+def get_commit_metrics_csv(
+    file: UploadFile = File(...),
+    db: Session = Depends(get_db),
+) -> List[CommitMetrics]:
+    file_content = io.TextIOWrapper(file.file, encoding="utf-8")
+    get_csv_commit_metrics_use_case = set_get_csv_commit_metrics_dependencies(db)
+    response = get_csv_commit_metrics_use_case.execute(file_content)
     return response
 
 
@@ -122,6 +136,14 @@ def set_get_copilot_metrics_dependencies(
         copilot_chat_metrics_repository,
         github_copilot_consumer,
     )
+
+
+def set_get_csv_commit_metrics_dependencies(
+    db: Session,
+) -> GetCsvCommitMetricsUseCase:
+    commit_metrics_repository = RawCommitMetricsRepository(db)
+    git_commit_metrics_csv_consumer = GitCommitMetricsCsvConsumer()
+    return GetCsvCommitMetricsUseCase(commit_metrics_repository, git_commit_metrics_csv_consumer)
 
 
 def set_get_calculated_metrics_dependencies(
---
+++ src/consumers/git_metrics_csv/git_metrics_csv_consumer.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from typing import List, TextIO
+import csv
+import uuid
+from src.domain.entities.commit_metrics import CommitMetrics
+from src.domain.entities.value_objects.author import Author
+from src.domain.entities.value_objects.repository import Repository
+
+
+class GitCommitMetricsCsvConsumer:
+  def execute(self, file_content: TextIO) -> List[CommitMetrics]:
+    metrics: List[CommitMetrics] = []
+
+    reader = csv.DictReader(file_content)
+    for row in reader:
+        commit = CommitMetrics(
+            id=str(uuid.uuid4()),
+            hash=row["hash"],
+            repository=Repository(name=row["repository"], team=""),
+            date=datetime.fromisoformat(row["date"]),
+            author=Author(name=row["author"], teams=[]),
+            language=row["language"],
+            added_lines=int(row["added_lines"]),
+            removed_lines=int(row["removed_lines"])
+        )
+        metrics.append(commit)
+
+    return metrics
