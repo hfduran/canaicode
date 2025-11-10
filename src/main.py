5686462f4aeb5403bf1118fd44a24f2723cda8cb
Henrique Duran <henriqueduran15@gmail.com>
2025-11-10 00:37:25 -0300

    fix build warnings (again)

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from fastapi.openapi.utils import get_openapi
 from src.infrastructure.logger.logger_config import logger
 from contextlib import asynccontextmanager
 from typing import Any
@@ -45,3 +46,23 @@ app.add_middleware(
 )
 
 app.include_router(router)
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Custom title",
+        version="2.5.0",
+        summary="This is a very custom OpenAPI schema",
+        description="Here's a longer description of the custom **OpenAPI** schema",
+        routes=app.routes,
+        openapi_version="3.0.0"
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
+
